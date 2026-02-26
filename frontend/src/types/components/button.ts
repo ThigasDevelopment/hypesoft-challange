b779c26292504746e6f8b6394bd154994f127592
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 03:05:49 -0300

    ♻️ refactor(button): remove Button component and associated styles

--- frontend/src/types/components/button.ts
+++
@@ -1,6 +0,0 @@
-import { style } from '@/components/ui/button/style';
-
-import { type ButtonHTMLAttributes } from 'react';
-import { type VariantProps } from 'class-variance-authority';
-
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof style>;
