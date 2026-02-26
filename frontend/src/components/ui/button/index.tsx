4edbfc71db1026a4f2db59dd9dead715ef2e0dba
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 03:19:29 -0300

    ✨ feat(button): implement Button component with styles and props

---
+++ frontend/src/components/ui/button/index.tsx
@@ -0,0 +1,23 @@
+import { style } from './style';
+
+import { forwardRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
+import { type ButtonProps } from '@/types';
+import { cn } from '@/lib/utils';
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps> (
+	({ className, variant, size, asChild = false, ...props }, ref) => {
+		const Component = asChild ? Slot : 'button';
+
+		return (
+			<Component
+				ref = { ref }
+				className = { cn (style({ variant, size }), className) }
+				{ ...props }
+			/>
+		);
+	}
+);
+
+Button.displayName = 'Button';
