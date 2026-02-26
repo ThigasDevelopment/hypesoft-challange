5d8adc6badc00c7a4d64fff271b03a641ba57096
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 15:16:34 -0300

    ♻️ refactor(card): simplify Card component by removing unused props and styles

--- frontend/src/components/ui/card/index.tsx
+++ frontend/src/components/ui/card/index.tsx
@@ -1,48 +1,12 @@
-import { style } from './style';
-
-import type { CardProps, CardHeaderProps } from '@/types/components/card';
+import type { CardProps } from '@/types/components/card';
 import { cn } from '@/lib/utils';
 
-export function Card ({ className, size, ...props }: CardProps) {
+export function Card ({ className, ...props }: CardProps) {
 	return (
 		<div
 			data-slot = 'card'
-			data-size = { size }
 
-			className = { cn (style ({ size }), className) }
-			{ ...props }
-		/>
-	)
-}
-
-export function CardHeader ({ className, ...props }: CardHeaderProps) {
-	return (
-		<div
-			data-slot = 'card-header'
-
-			className = { cn ('flex flex-col space-y-1.5 p-6', className) }
-			{ ...props }
-		/>
-	)
-}
-
-export function CardTitle ({ className, ...props }: CardHeaderProps) {
-	return (
-		<h3
-			data-slot = 'card-title'
-
-			className = { cn ('text-lg font-semibold', className) }
-			{ ...props }
-		/>
-	)
-}
-
-export function CardContent ({ className, ...props }: CardHeaderProps) {
-	return (
-		<div
-			data-slot = 'card-content'
-
-			className = { cn ('px-4 group-data-[size=sm]/card:px-3', className) }
+			className = { cn ('rounded-xl border bg-card text-card-foreground shadow p-6', className) }
 			{ ...props }
 		/>
 	)
--- frontend/src/types/components/card.ts
+++ frontend/src/types/components/card.ts
@@ -1,7 +1,3 @@
 import { type ComponentProps } from 'react';
 
-export interface CardProps extends ComponentProps<'div'> {
-	size?: 'default' | 'sm';
-}
-
-export interface CardHeaderProps extends ComponentProps<'div'> { }
+export interface CardProps extends ComponentProps<'div'> { }
