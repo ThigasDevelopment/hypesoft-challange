6e212db8fcc36bc2bfc6fd47a715f80f8ad8bc05
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 04:42:27 -0300

    ✨ feat(routes): add login route and integrate AuthGuard for dashboard

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,17 +1,26 @@
+import { AuthGuard } from './auth';
+
 import { type AppRoute } from '@/types/index';
 
 import { DashboardLayout } from '@/components/layout/dashboard';
 
+import { Login } from '@/pages/login';
+
 export const routes: AppRoute[] = [
+	{
+		path: '/login',
+		element: <Login />
+	},
+
 	{
 		path: '/',
 		name: 'Dashboard',
-		element: <DashboardLayout />,
+		element: <AuthGuard />,
 
 		children: [
 			{
 				path: '/',
-				element: <h1>Dashboard</h1>
+				element: <DashboardLayout />
 			},
 		],
 	}
