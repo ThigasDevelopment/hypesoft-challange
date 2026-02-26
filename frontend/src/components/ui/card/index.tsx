d9948ebdc84376ab17829cab3cab3bc07d825fef
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 11:49:28 -0300

    ✨ feat(card): implement Card component with styles and props interface

--- frontend/src/components/ui/card/index.tsx
+++ frontend/src/components/ui/card/index.tsx
@@ -1,1 +1,16 @@
 import { style } from './style';
+
+import { type CardProps } from '@/types/components/card';
+import { cn } from '@/lib/utils';
+
+export function Card ({ className, size, ...props }: CardProps) {
+	return (
+		<div
+			data-slot = 'card'
+			data-size = { size }
+
+			className = { cn (style({ size }), className) }
+			{ ...props }
+		/>
+	)
+}
---
+++ frontend/src/types/components/card.ts
@@ -0,0 +1,5 @@
+import { type ComponentProps } from 'react';
+
+export interface CardProps extends ComponentProps<'div'> {
+	size?: 'default' | 'sm';
+}
