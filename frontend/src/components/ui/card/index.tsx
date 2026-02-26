40f5ef8843335a40310ca7a40cd4963e189dd460
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 12:02:14 -0300

    ✨ feat(card): add CardHeader, CardTitle, and CardContent components with props interface

--- frontend/src/components/ui/card/index.tsx
+++ frontend/src/components/ui/card/index.tsx
@@ -1,6 +1,6 @@
 import { style } from './style';
 
-import { type CardProps } from '@/types/components/card';
+import type { CardProps, CardHeaderProps } from '@/types/components/card';
 import { cn } from '@/lib/utils';
 
 export function Card ({ className, size, ...props }: CardProps) {
@@ -9,7 +9,40 @@ export function Card ({ className, size, ...props }: CardProps) {
 			data-slot = 'card'
 			data-size = { size }
 
-			className = { cn (style({ size }), className) }
+			className = { cn (style ({ size }), className) }
+			{ ...props }
+		/>
+	)
+}
+
+export function CardHeader ({ className, ...props }: CardHeaderProps) {
+	return (
+		<div
+			data-slot = 'card-header'
+
+			className = { cn ('flex flex-col space-y-1.5 p-6', className) }
+			{ ...props }
+		/>
+	)
+}
+
+export function CardTitle ({ className, ...props }: CardHeaderProps) {
+	return (
+		<h3
+			data-slot = 'card-title'
+
+			className = { cn ('text-lg font-semibold', className) }
+			{ ...props }
+		/>
+	)
+}
+
+export function CardContent ({ className, ...props }: CardHeaderProps) {
+	return (
+		<div
+			data-slot = 'card-content'
+
+			className = { cn ('px-4 group-data-[size=sm]/card:px-3', className) }
 			{ ...props }
 		/>
 	)
--- frontend/src/types/components/card.ts
+++ frontend/src/types/components/card.ts
@@ -3,3 +3,5 @@ import { type ComponentProps } from 'react';
 export interface CardProps extends ComponentProps<'div'> {
 	size?: 'default' | 'sm';
 }
+
+export interface CardHeaderProps extends ComponentProps<'div'> { }
