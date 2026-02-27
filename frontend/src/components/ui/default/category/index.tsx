36bed74885f9f4d9a307a791b12900c320154be5
ThigasDevelopment <maia.thico@gmail.com>
2026-02-27 15:58:15 -0300

    💎 style(category): update Category component layout for improved structure and readability

--- frontend/src/components/ui/default/category/index.tsx
+++ frontend/src/components/ui/default/category/index.tsx
@@ -1,11 +1,12 @@
 import type { CategoryProps } from '@/types';
 
-import { Card } from '../../index';
+import { Button, Card } from '../../index';
 
 export function Category ({ name }: CategoryProps) {
 	return (
-		<Card className = 'w-full'>
-
+		<Card className = 'flex w-full items-center justify-between h-16'>
+			<h1 className = 'text-sm'>{ name }</h1>
+			<Button variant = 'destructive'>Excluir</Button>
 		</Card>
 	)
 }
