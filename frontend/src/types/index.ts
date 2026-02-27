3d8d5679fb89aad08b5ed59d8993c60f3928f7ac
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 22:13:42 -0300

    ♻️ refactor(types): remove unused dropdown export from index.ts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,6 +1,5 @@
 export * from './components/button';
 export * from './components/card';
-export * from './components/dropdown';
 export * from './components/input';
 
 export * from './router/routes';
