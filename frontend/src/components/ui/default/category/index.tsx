29d2c6c8a8fa0772615933ca2db49b0aba64e0b6
ThigasDevelopment <maia.thico@gmail.com>
2026-02-27 16:02:45 -0300

    ✨ feat(category): add date property to Category component and display creation date

--- frontend/src/components/ui/default/category/index.tsx
+++ frontend/src/components/ui/default/category/index.tsx
@@ -2,10 +2,10 @@ import type { CategoryProps } from '@/types';
 
 import { Button, Card } from '../../index';
 
-export function Category ({ name }: CategoryProps) {
+export function Category ({ name, date }: CategoryProps) {
 	return (
 		<Card className = 'flex w-full items-center justify-between h-16'>
-			<h1 className = 'text-sm'>{ name }</h1>
+			<h1 className = 'text-sm'>{ name }<p className = 'text-sm'>Criado em: { date } </p></h1>
 			<Button variant = 'destructive'>Excluir</Button>
 		</Card>
 	)
--- frontend/src/types/components/category.ts
+++ frontend/src/types/components/category.ts
@@ -2,4 +2,5 @@ import type { ComponentProps } from 'react';
 
 export interface CategoryProps extends ComponentProps<'div'> {
 	name: string;
+	date: string;
 }
