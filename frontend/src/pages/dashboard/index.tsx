e99dfb1244e0aa4b46835be5c889f03e7fc03acb
ThigasDevelopment <maia.thico@gmail.com>
2026-02-26 23:12:00 -0300

    ✨ feat(dashboard): reintroduce 'Produtos por categoria' chart in Dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -83,39 +83,6 @@ export function Dashboard() {
 					<Card>
 						<div className = 'flex items-center justify-between px-2'>
 							<h1 className = 'text-lg font-bold'>
-								Produtos por categoria
-							</h1>
-
-							<Layers className = 'h-6 w-6 text-muted-foreground'/>
-						</div>
-
-						<div className = 'px-2 pt-4 flex-1 h-75 w-full min-h-75'>
-							<ResponsiveContainer width = '100%' height = '100%'>
-								<BarChart data = { dummyCategoryData }>
-									<CartesianGrid strokeDasharray = '3 3' stroke = 'var(--border-color)'/>
-
-									<XAxis dataKey = 'name' tick = { { fill: 'var(--text-color)' } }/>
-									<YAxis tick = { { fill: 'var(--text-color)' } }/>
-
-									<Tooltip
-										contentStyle = {
-											{ background: 'var(--bg-color)', borderRadius: '10px' }
-										}
-
-										itemStyle = {
-											{ color: 'var(--primary-color)' }
-										}
-									/>
-									
-									<Bar dataKey = 'amount' name = 'Quantidade' barSize = { 15 } fill = 'var(--primary-color)' activeBar = { { fill: 'var(--primary-hover)' } } />
-								</BarChart>
-							</ResponsiveContainer>
-						</div>
-					</Card>
-
-					<Card>
-						<div className = 'flex items-center justify-between px-2'>
-							<h1 className = 'text-lg font-bold'>
 								Produtos com estoque baixo
 							</h1>
 
@@ -152,6 +119,39 @@ export function Dashboard() {
 									)
 								)
 							}
+						</div>
+					</Card>
+					
+					<Card>
+						<div className = 'flex items-center justify-between px-2'>
+							<h1 className = 'text-lg font-bold'>
+								Produtos por categoria
+							</h1>
+
+							<Layers className = 'h-6 w-6 text-muted-foreground'/>
+						</div>
+
+						<div className = 'px-2 pt-4 flex-1 h-75 w-full min-h-75'>
+							<ResponsiveContainer width = '100%' height = '100%'>
+								<BarChart data = { dummyCategoryData }>
+									<CartesianGrid strokeDasharray = '3 3' stroke = 'var(--border-color)'/>
+
+									<XAxis dataKey = 'name' tick = { { fill: 'var(--text-color)' } }/>
+									<YAxis tick = { { fill: 'var(--text-color)' } }/>
+
+									<Tooltip
+										contentStyle = {
+											{ background: 'var(--bg-color)', borderRadius: '10px' }
+										}
+
+										itemStyle = {
+											{ color: 'var(--primary-color)' }
+										}
+									/>
+									
+									<Bar dataKey = 'amount' name = 'Quantidade' barSize = { 15 } fill = 'var(--primary-color)' activeBar = { { fill: 'var(--primary-hover)' } } />
+								</BarChart>
+							</ResponsiveContainer>
 						</div>
 					</Card>
 				</div>
