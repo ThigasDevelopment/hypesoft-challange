6dc93726a54119b010c8ab3941d358e58708b28b
ThigasDevelopment <maia.thico@gmail.com>
2026-03-03 01:46:10 -0300

    💎 style(dashboard): update layout for header to improve responsiveness

--- frontend/src/pages/private/dashboard/index.tsx
+++ frontend/src/pages/private/dashboard/index.tsx
@@ -33,7 +33,7 @@ export function Dashboard() {
 
   	return (
 		<div className = 'space-y-6'>
-			<div className = 'flex items-center justify-between'>
+			<div className = 'flex flex-col sm:flex-row items-center justify-between gap-4'>
 				<h2 className = 'text-2xl font-bold'>
 					Visão Geral
 				</h2>
